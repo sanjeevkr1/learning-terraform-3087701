2da442cff7f9ea593d85b420fdeecb3a6341ec47
sanjeevkr <kumar.sanjeev.pune@gmail.com>
2023-05-03 17:53:13 +0530

    commit 1 to use vars and output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,10 @@
 #output "instance_arn" {
 #  value = aws_instance.web.arn
 #}
+
+output "instance_ami"{
+    value=aws_instance.web.ami
+}
+output "instance_arn"{
+    value=aws_instance.web.arn
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+
+variable "instance_type"{
+    description="Type of EC2 Instance"
+    default="t3.nano"
+}
