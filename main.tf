35fb3b70bf5225cd51371d9541da5ab97112e02f
sanjeevkr <kumar.sanjeev.pune@gmail.com>
2023-05-04 20:36:19 +0530

    test

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc_web.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
+  #map_public_ip_on_launch = true
   tags = {
     Name = "tf-example"
   }
