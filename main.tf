98eaa47526df20fd9bb3baad42d68fc6b55e283b
sanjeevkr <kumar.sanjeev.pune@gmail.com>
2023-05-04 20:37:13 +0530

    test2

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc_web.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "us-west-2a"
-  #map_public_ip_on_launch = true
+  map_public_ip_on_launch = true
   tags = {
     Name = "tf-example"
   }
@@ -42,7 +42,7 @@ resource "aws_subnet" "my_subnet" {
 
 resource "aws_network_interface" "net_interface" {
   subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+  #private_ips = ["172.16.10.100"]
   security_groups = [aws_security_group.my_sg_web_allow_http_https_in_allow_all_out.id]
   tags = {
     Name = "primary_network_interface"
